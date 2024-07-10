cbc7ed22f6b99c3ecfe0489aac832f09a461b6a8
Moises Villalba Silvero <moisesvillalba@gmail.com>
2024-07-10 17:04:03 -0400

    dia 1 - mouredev

---
+++ Basic/00-helloworld.js
@@ -0,0 +1,4 @@
+console.log("Hola JavaScipt con comillas dobles!")
+console.log("5")
+console.log(5 + 2)
+console.log('Hola JavaScipt! con comillas simples')
---
+++ Basic/01-variables.js
@@ -0,0 +1,19 @@
+//var
+
+var helloWorld = "Hola mundo variable var"
+console.log(helloWorld)
+
+helloWorld   =  "Hola, de nuevo.. Javascript"
+console.log(helloWorld)
+
+//let
+let helloWorldLet = "Hola mundo variable let"
+console.log(helloWorldLet)
+
+helloWorldLet = "Hola, de nuevo.. Javascript 2"
+console.log(helloWorldLet)
+
+//const
+//cambios
+const helloWorldconst = "Hola mundo variable const"
+console.log(helloWorldconst)
